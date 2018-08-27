f6fe5aa66131113f15b19bac2f459b4a361e091e
Mahdi <mahdisafari20002013@gmail.com>
2018-08-27 04:16:24 -0700

    Edit icon sidebar and edit patient list

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -5,15 +5,15 @@
             <ul class="nav metismenu" id="side-menu">
                 <li class="nav-header">
                     <div class="dropdown profile-element"> <span>
-                            <img alt="image" class="img-circle" src="img/icon_co.png" />
+                            <img alt="image" class="img-circle" src="img/icon_co.png" width="50px"/>
                              </span>
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">HK Dental Clinic</strong>
                              </span> <span class="text-muted text-xs block">Profile <b class="caret"></b></span> </span> </a>
                         <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                            <li><a href="#">Profile</a></li>
-                            <li><a href="#">Contacts</a></li>
-                            <li><a href="#">Mailbox</a></li>
+                            {{--<li><a href="#">Profile</a></li>--}}
+                            {{--<li><a href="#">Contacts</a></li>--}}
+                            {{--<li><a href="#">Mailbox</a></li>--}}
                             <li class="divider"></li>
                             <li><a href="#">Logout</a></li>
                         </ul>
@@ -23,13 +23,13 @@
                     </div>
                 </li>
                 <li class="active">
-                    <a href="/"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
+                    <a href="/"><i class="fa fa-home"></i> <span class="nav-label">Home</span> </a>
 
                 </li>
                 <li>
-                    <a href="/doctor_operations"><i class="fa fa-diamond"></i> <span class="nav-label">Department</span></a>
+                    <a href="/doctor_operations"><i class="fa fa-sitemap"></i> <span class="nav-label">Department</span><span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level collapse">
-                        <li><a href="/doctor_operations">Treatment</a></li>
+                        <li><a href="/doctor_operations">Doctor</a></li>
                         <li><a href="/xray">X-Ray</a></li>
                     </ul>
                 </li>
@@ -38,16 +38,16 @@
                     {{--<a href="/doctor"><i class="fa fa-envelope"></i> <span class="nav-label">Doctors </span><span class="label label-warning pull-right">16/24</span></a>--}}
                 {{--</li>--}}
                 <li>
-                    <a href="/patient"><i class="fa fa-pie-chart"></i> <span class="nav-label">Patient</span>  </a>
+                    <a href="/patient"><i class="fa fa-user-plus"></i> <span class="nav-label">Patient Registeration</span>  </a>
                 </li>
                 <li>
-                    <a href="/appo"><i class="fa fa-flask"></i> <span class="nav-label">List Appointment</span></a>
+                    <a href="/appo"><i class="fa fa-list-alt"></i> <span class="nav-label">List Patients</span></a>
                 </li>
                 <li>
-                    <a href="/reception"><i class="fa fa-flask"></i> <span class="nav-label">Reception</span></a>
+                    <a href="/reception"><i class="fa fa-money"></i> <span class="nav-label">Reception</span></a>
                 </li>
                 <li>
-                    <a href="setting"><i class="fa fa-edit"></i> <span class="nav-label">Setting</span><span class="fa arrow"></span></a>
+                    <a href="setting"><i class="fa fa-cog"></i> <span class="nav-label">Setting</span><span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level collapse">
                         <li><a href="account">Accounts</a></li>
                         <li><a href="#">Backup Database </a></li>
@@ -55,14 +55,14 @@
 
                 </li>
                 <li>
-                    <a href="#"><i class="fa fa-desktop"></i> <span class="nav-label">Reports</span>  <span class="pull-right label label-primary">SPECIAL</span></a>
+                    <a href="#"><i class="fa fa-file"></i> <span class="nav-label">Reports</span>  <span class="pull-right label label-primary">SPECIAL</span></a>
                     <ul class="nav nav-second-level collapse">
                         <li><a href="#">Patients</a></li>
                         <li><a href="#">Doctors</a></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="#"><i class="fa fa-files-o"></i> <span class="nav-label">Finance</span><span class="fa arrow"></span></a>
+                    <a href="#"><i class="fa fa-clipboard"></i> <span class="nav-label">Finance</span><span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level collapse">
                         <li><a href="#">Expenditure</a></li>
                         <li><a href="#">Income</a></li>
--- routes/web.php
+++ routes/web.php
@@ -30,43 +30,57 @@ Route::get('/reception', function () {
     return view('reception');
 });
 
+
 Route::get('/detailinvoice', function () {
     return view('detailsReception');
 });
 
-
+// doctor treatment operation page have three option
 Route::get('/doctor_operations', function () {
     return view('doctor_operations');
 });
 
+// X-Ray page
 Route::get('/xray', function () {
     return view('Xrey_dep');
 });
 
+// medicine page in treatement -> medicine route
 Route::get('/medicine', function () {
     return view('medicine');
 });
 
+// next appointment page
 Route::get('/next_appointment', function () {
     return view('next_appointment');
 });
+
+// full calandar show next Visit appointment with patient
 Route::get('fullcalender',function (){
    return view('fullcalender');
 });
-Route::get('test',"CalenderController@index");
-Route::get('iframe',function (){
-   return view('iframe') ;
+
+// iframe take own src from fullCalendar do not delete fullcalendar
+Route::get('/iframe',function (){
+   return view('/iframe') ;
 });
+
+// login of users
 Route::get('login',function (){
     return view('login');
 });
 
+// dashboard of clinic
 Route::get('/dash',function (){
     return view('dash');
 });
+
+// show all account users
 Route::get('account',function (){
    return view('account');
 });
+
+// create new users
 Route::get('create_account',function(){
    return view('create_account');
 });

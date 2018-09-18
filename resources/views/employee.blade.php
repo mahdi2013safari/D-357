1d89977b586d6e442e235e50f820ccafd39b6132
Mohammad-jawad <sakhizadah1468@gmail.com>
2018-09-18 14:43:10 +0430

    doctor registration modified

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -1,12 +1,16 @@
 @extends('master')
 
+@section('style')
 
+
+
+@endsection
 @section('content')
 
     <div class="col-lg-12">
         <div class="ibox float-e-margins">
             <div class="ibox-title">
-                <h5>Employee Registration </h5>
+                <h5>Doctor Registration </h5>
                 <div class="ibox-tools">
                     <a class="collapse-link">
                         <i class="fa fa-chevron-up"></i>
@@ -31,13 +35,13 @@
                     <form method="get" class="form-horizontal">
 
                         <div class="form-group">
-                            <label class="col-sm-2 control-label">Emp-ID</label>
-                            <div class="col-md-4"><input type="text" class="form-control" placeholder="Emp-id"></div>
+                            <label class="col-sm-2 control-label">Doctor-ID</label>
+                            <div class="col-md-4"><input type="text" class="form-control" placeholder="Enter DoctorID"></div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Full name</label>
-                            <div class="col-md-4"><input type="text" class="form-control" placeholder="Emp-Name"></div>
+                            <div class="col-md-4"><input type="text" class="form-control" placeholder="Enter Name of Doctor"></div>
                         </div>
 
 
@@ -48,7 +52,7 @@
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Working Time</label>
                             <div class="col-md-4">
-                                <input type="time"  />
+                                <label for=""> From </label><input type="time" class="form-control" style="width: 40%"> <label for=""> To </label> <input type="time" class="form-control" style="width: 40%">
                             </div>
                         </div>
 
@@ -60,7 +64,7 @@
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Departement</label>
                             <div class="col-md-4">
-                                <select class="select2_demo_1 form-control" id="dept"disabled>
+                                <select class="select2_demo_1 form-control" id="dept">
                                     <option value="1">Select Department</option>
                                     <option value="1">regular basis</option>
                                     <option value="2">Orthodontist</option>
@@ -81,14 +85,14 @@
 
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Salary Type</label>
-                            <div class="col-sm-2"><div class="i-checks"><label> <input type="checkbox" value="option2" name="a2"> <i></i> Per% <input type="text"class="form-control"placeholder="percentage" style="float: right;"></label></div></div>
-                            <div class="col-sm-2"><div class="i-checks"><label> <input type="checkbox" value="option2" name="a2"> <i></i>  Fix </label></div></div>
-                            <div class="col-sm-2"><div class="i-checks"><label> <input type="checkbox" value="option2" name="a2"> <i></i>  Monthly</label></div></div>
-                            <div class="col-sm-2"><div class="i-checks"><label> <input type="checkbox" value="option2" name="a2"> <i></i>  Daily </label></div></div>
+                            <div class="col-sm-2"> <div class="input-group" style="display: inline"><div class="i-checks"><label> <input type="radio" value="option2" name="a2" id="per"> <i></i> Per% </label></div></div></div>
+                            <div class="col-sm-2"><div class="i-checks"><label> <input type="radio" value="option2" name="a2"> <i></i>  Fix </label></div></div>
+                            {{--<div class="col-sm-2"><div class="i-checks"><label> <input type="checkbox" value="option2" name="a2"> <i></i>  Monthly</label></div></div>--}}
+                            {{--<div class="col-sm-2"><div class="i-checks"><label> <input type="checkbox" value="option2" name="a2"> <i></i>  Daily </label></div></div>--}}
                         </div>
 
                         <div class="form-group"><label class="col-sm-2 control-label">Salary amount</label>
-                            <div class="col-md-4"><input type="number" class="form-control" placeholder="Salary amount"></div>
+                            <div class="col-md-4"><input type="text" class="form-control" id="sal" placeholder="Enter amount or percentage of salary"></div>
                         </div>
 
                         <div class="form-group">
@@ -136,19 +140,18 @@
 
                                 <div class="row">
                                     <div class="col-md-7">
-                                        <div class="input-group"><input type="text" placeholder="Search Doctor name" class="input-sm form-control"> <span class="input-group-btn">
-                                        <button type="button" class="btn btn-sm btn-primary"> Go!</button> </span></div>
+                                        <div class="input-group"> <span class="input-group-btn"><button type="button" class="btn btn-sm btn-primary"> Go!</button> </span>
+                                            <input type="text" placeholder="Search Doctor name" class="input-sm form-control">
+                                        </div>
                                     </div>
                                 </div><br/>
                                 <div class="table-responsive">
                                     <table class="table table-striped table-bordered table-hover dataTables-example" >
                                         <thead>
                                         <tr>
-                                            <th>Emp-ID</th>
+                                            <th>Doctor-ID</th>
                                             <th>Name</th>
-                                            <th>Type of job</th>
-                                            <th>Grade</th>
-                                            <th>Work Time</th>
+                                            <th>Working time</th>
                                             <th>Phone</th>
                                             <th>Departement</th>
                                             <th>Gender</th>
@@ -160,53 +163,43 @@
                                         <tr class="gradeX">
                                             <td>D-434</td>
                                             <td>Dr.Ahmadi</td>
-                                            <td>Doctor</td>
-                                            <td>Master</td>
-                                            <td>Full time</td>
+                                            <td>08am-04pm</td>
                                             <td>0745544432</td>
                                             <td class="center">Orthodontist</td>
                                             <td class="center">Male</td>
-                                            <td>Fix-Monthly</td>
+                                            <td>Fix</td>
                                             <td class="center">15000 Afg</td>
                                         </tr>
                                         <tr class="gradeX">
                                             <td>D-435</td>
                                             <td>Dr.Sadiq</td>
-                                            <td>Doctor</td>
-                                            <td>Master</td>
-                                            <td>Two days per week</td>
-                                            <td>0746743432</td>
+                                            <td>08am-04pm</td>
+                                            <td>0745544432</td>
                                             <td class="center">Orthodontist</td>
                                             <td class="center">Male</td>
-                                            <td>Per% 75</td>
-                                            <td class="center">34000Afg</td>
+                                            <td>Fix</td>
+                                            <td class="center">15000 Afg</td>
                                         </tr>
                                         <tr class="gradeX">
-                                            <td>N-124</td>
-                                            <td>Raziq</td>
-                                            <td>Nurse</td>
-                                            <td>////</td>
-                                            <td>Half Day</td>
-                                            <td>0745532132</td>
-                                            <td class="center">Endodontist</td>
+                                            <td>D-436</td>
+                                            <td>Dr.Samim</td>
+                                            <td>08am-12pm</td>
+                                            <td>0745544432</td>
+                                            <td class="center">Orthodontist</td>
                                             <td class="center">Male</td>
-                                            <td>Fix-Monthly</td>
-                                            <td class="center">9000 Afg</td>
+                                            <td>percentage</td>
+                                            <td class="center">45%</td>
                                         </tr>
-
                                         <tr class="gradeX">
-                                            <td>G-624</td>
-                                            <td>Akhtar</td>
-                                            <td>Guard</td>
-                                            <td>////</td>
-                                            <td>Full Time</td>
-                                            <td>0748573132</td>
-                                            <td class="center">General</td>
+                                            <td>D-437</td>
+                                            <td>Dr.Jalal</td>
+                                            <td>12pm-03pm</td>
+                                            <td>0745544432</td>
+                                            <td class="center">Orthodontist</td>
                                             <td class="center">Male</td>
-                                            <td>Fix-Monthly</td>
-                                            <td class="center">1000 Afg</td>
+                                            <td>percentage</td>
+                                            <td class="center">32%</td>
                                         </tr>
-
                                     </table>
                                 </div>
 
@@ -222,22 +215,3 @@
 
 @endsection
 
-@section('script')
-    <script type="text/javascript">
-        function disabling(){
-            var j=document.getElementById("job").value;
-            //Grade specification
-            if (j!=1) {
-                document.getElementById("grade").disabled=true;
-            }else {
-                document.getElementById("grade").disabled=false;
-            }
-            // Department specification
-            if (j!=1) {
-                document.getElementById("dept").disabled=true;
-            }else {
-                document.getElementById("dept").disabled=false;
-            }
-        }
-    </script>
-@endsection

cec045d7e17f40cf321afbe1e3a1f123225082e3
mahdisafari <mahdisafari20002013@gmail.com>
2018-10-08 22:51:04 -0700

    Create table seeder for Treatment

--- app/Treatment.php
+++ app/Treatment.php
@@ -6,8 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Treatment extends Model
 {
-
+//    protected $dateFormat = 'd.m.Y';
+//    protected $dates = ['created_at', 'updated_at', 'published_at', 'deleted_at'];
+    // etc...
     protected $guarded = [];
+
     public function patient()
     {
         return $this->belongsTo(Patient::class, 'patient_id', 'id');
---
+++ database/seeds/TreatmentTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Treatment;
+use Illuminate\Database\Seeder;
+
+class TreatmentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Treatment::class, 50)->create();
+    }
+}

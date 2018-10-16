3487d383f860bc3dade9ffd8713d9def65f77298
Mohammad-jawad <sakhizadah1468@gmail.com>
2018-10-16 13:48:21 +0430

    alert is set to browser alert

---
+++ resources/views/layout/messages.blade.php
@@ -0,0 +1,23 @@
+@if(count($errors)>0)
+    @foreach($errors->all() as $error)
+        {{$error}}
+    @endforeach
+@endif
+
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible text-center" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <h2>{{session('success')}}</h2>
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger alert-dismissible text-center" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <h2>{{session('error')}}</h2>
+    </div>
+@endif

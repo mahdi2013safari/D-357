3f06e1ef13d562b6933531b9eeccafa83af940be
Mohammad-jawad <sakhizadah1468@gmail.com>
2018-10-25 22:14:21 +0430

    employee form modified

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -97,52 +97,50 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-12">
-                                    <div class="col-sm-3">
-                                        <div class="form-group"><label class="control-label">{{trans('file.max_patient')}}</label>
+                                <div class="col-sm-5">
+                                        <div class="form-group" style="margin-left:1px;"><label class="control-label">{{trans('file.max_patient')}}</label>
                                             <input type="number" max="100" class="form-control" name="max_patient" placeholder="{{trans('file.max_patient')}}" required>
                                         </div>
-                                    </div>
-                                    <div class="col-sm-1"></div>
-                                    <div class="col-sm-3">
-                                        <div class="form-group">
+                                        <div class="form-group" style="margin-left:1px;">
                                             <label class=" control-label">{{trans('file.email')}}</label>
                                             <input type="email" class="form-control" name="email" placeholder="{{trans('file.email')}}"  required>
                                         </div>
-                                    </div>
-                                    <div class="col-sm-1"></div>
-                                    <div class="col-sm-3">
-                                        <div class="form-group">
+                                        <div class="form-group" style="margin-left:1px;">
                                             <label class=" control-label">{{trans('file.password')}}</label>
                                             <input type="password" class="form-control" name="password" placeholder="{{trans('file.password')}}" required>
                                         </div>
-                                    </div>
                                 </div>
-                                <div class="col-sm-12" style="margin-top: 18px;">
-                                    <div class="col-sm-3 text-center" style="margin-top: 40px">
+                                <div class="col-sm-1"></div>
+                                <div class="col-sm-5" style="margin-left: -25px;">
                                         <div class="form-group">
-                                          <label class=" control-label" style="letter-spacing: 2px;line-height: 12px;font-size: 18px">{{trans('file.working_time')}}: <i class="fa fa-arrow-right"></i></label>
+                                            <div class="col-sm-5" style="margin-top: 22px;margin-left: 10px;">
+                                          <label class=" control-label" style="letter-spacing: 2px;line-height: 12px;font-size: 18px">{{trans('file.working_time')}}:</label>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-sm-1"></div>
-                                    <div class="col-sm-3">
+                                        <div class="col-sm-12" style="">
                                         <div class="form-group">
-                                           <label style="line-height: 35px;">{{trans('file.from')}} </label>
-                                           <input type="time" class="form-control" name="start_work_time" style="width: 40%" required>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-1"></div>
-                                    <div class="col-sm-3">
-                                       <div class="form-group">
+                                            <div class="col-sm-5">
+                                        <label style="line-height: 35px;">{{trans('file.from')}} </label>
+                                        <input type="time" class="form-control" name="start_work_time"  required>
+                                            </div>
+                                            <div class="col-sm-5">
                                         <label style="line-height: 35px;">{{trans('file.to')}}</label>
-                                        <input type="time" class="form-control" name="end_work_time" style="width: 40%" required>
+                                        <input type="time" class="form-control" name="end_work_time"  required>
+                                            </div>
                                        </div>
+                                        </div>
+                                </div>
+                                <div class="col-sm-5" style="margin-left: 85px;margin-top: 30px">
+                                    <div class="form-group">
+                                        <div class="col-sm-3"><label class="control-label">{{trans('file.gender')}}:</label></div>
+                                        <div class="col-sm-3" style="margin-left: 15px;"><div class="i-checks"><label><input type="radio" value="Male" name="gender"  required>&nbsp;&nbsp;{{trans('file.male')}}</label></div></div>
+                                        <div class="col-sm-3"><div class="i-checks"><label> <input type="radio" value="Female" name="gender" required>&nbsp;&nbsp; {{trans('file.female')}}</label></div></div>
                                     </div>
                                 </div>
                                 <div class="col-sm-12" style="margin-top: 20px;">
                                     <div class="col-sm-5 text-center" style="margin-top: 22px;">
                                         <div class="form-group">
-                                            <div class="col-sm-3"><label class=" control-label">{{trans('file.salary_type')}}:</label></div>
+                                            <div class="col-sm-3" style="margin-left: -17px;"><label class=" control-label">{{trans('file.salary_type')}}:</label></div>
                                             <div class="col-sm-3"><label><input type="radio" name="salary_type" value="percentage" id="per" onclick="perSal()" style="height: 22px;width:22px;" required><i></i>{{trans('file.per')}}%</label></div>
                                             <div class="col-sm-3"><label><input type="radio" name="salary_type" onclick="fixSal()" id="fix" value="fix" style="height: 22px;width:22px;"> <i></i>{{trans('file.fix')}}</label></div>
                                         </div>
@@ -151,23 +149,17 @@
                                     <div class="col-sm-5 text-left">
                                         <div class="form-group">
                                             <label class=" control-label" id="label">{{trans('file.salary_amount')}}</label>
-                                            <input type="number" class="form-control" name="salary_amount" id="sal" required>
+                                            <input type="number" class="form-control" name="salary_amount" id="sal" disabled required>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-sm-12" style="margin-top: 25px;">
-                                <div class="col-sm-5">
-                                <div class="form-group">
-                                    <div class="col-sm-3"><label class="control-label">{{trans('file.gender')}}:</label></div>
-                                    <div class="col-sm-3" style="margin-left: 15px;"><div class="i-checks"><label><input type="radio" value="Male" name="gender"  required><i></i>{{trans('file.male')}}</label></div></div>
-                                    <div class="col-sm-3"><div class="i-checks"><label> <input type="radio" value="Female" name="gender" required><i></i> {{trans('file.female')}}</label></div></div>
-                                </div>
-                                </div>
+
                                 <div class="col-sm-1"></div>
-                                <div class="col-sm-5 text-right">
+                                <div class="col-sm-12 text-right">
                                 <div class="form-group">
-                                        <button class="btn btn-white"  type='reset'>{{trans('file.reset')}}</button>&nbsp;
-                                        <button class="btn btn-primary " type="submit" name="submit" value="Save">{{trans('file.save')}}
+                                        <button class="btn btn-white"  type='reset' >{{trans('file.reset')}}</button>&nbsp;
+                                        <button class="btn btn-primary " type="submit" name="submit" value="Save" style="margin-right: 93px">{{trans('file.save')}}
                                         </button>
                                 </div>
                                 </div>
@@ -190,12 +182,14 @@
             document.getElementById('label').innerHTML = 'Salary Percentage';
             document.getElementById('sal').setAttribute('max', 100);
             document.getElementById('sal').setAttribute('placeholder', 'Enter the percentage of salary');
+            document.getElementById('sal').disabled=false;
         }
 
         function fixSal() {
             document.getElementById('label').innerHTML = 'Salary amount';
             document.getElementById('sal').setAttribute('max', 10000000);
             document.getElementById('sal').setAttribute('placeholder', 'Enter the amount  of salary');
+            document.getElementById('sal').disabled=false;
         }
     </script>
 

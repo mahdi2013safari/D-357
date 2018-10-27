015215e160d392dca8a623a803e893053766e23a
mahdisafari <mahdisafari20002013@gmail.com>
2018-10-27 16:04:23 +0430

    add new icon and edit next appointment

--- resources/views/part/nav_doctor.blade.php
+++ resources/views/part/nav_doctor.blade.php
@@ -1,30 +1,20 @@
 
 <div class="row" style="margin-left:2px;margin-right: 20px;">
     <div class="col-sm-3">
-        <h2><a class="btn btn-primary hvr-grow-shadow" style="font-size: 13px; width: 200px;"
-               href="/operation"><i class="fa fa-table pull-left" style="color:#ffc000; font-size: 25px;"></i>&nbsp;&nbsp;Patient List</a>
+        <h2 class="title-button"><a class="btn btn-primary hvr-grow-shadow" style="font-size: 13px; width: 200px;"
+               href="/operation"><img src="{{ asset('img/patient_list.png') }}" class="pull-left title-button" width="40px"/>&nbsp;&nbsp;Patient List</a>
         </h2>
     </div>
 
-    {{--<div class="col-sm-3">--}}
-        {{--<h2><a class="btn btn-primary hvr-grow-shadow" style="font-size: 13px; width: 200px;"--}}
-               {{--href="/treatment_operation"><i class="fa fa-stethoscope pull-left"--}}
-                                              {{--style="color:#ffc000; font-size:25px;"></i>&nbsp;&nbsp;Treatment--}}
-                {{--operation</a></h2>--}}
-    {{--</div>--}}
 
     <div class="col-sm-3">
-        <h2><a class="btn btn-primary hvr-grow-shadow" style="font-size: 13px; width: 200px;"
-               href="/next_appointment_list"><i class="fa fa-calendar pull-left"
-                                                style="color:#ffc000; font-size: 25px;"></i>&nbsp;&nbsp;Next
-                appointment List</a></h2>
+        <h1><a class="btn btn-primary hvr-grow-shadow" style="font-size: 13px; width: 200px;"
+               href="/next_appointment_list"><img src="{{ asset('img/patient_appointment.png') }}" class="pull-left" width="40px"/>&nbsp;&nbsp;Next appointment List</a></h1>
     </div>
 
     <div class="col-sm-3">
         <h2><a class="btn btn-primary hvr-grow-shadow" style="font-size: 13px; width: 200px;"
-               href="/patient_history_print"><i class="fa fa-file-pdf-o pull-left"
-                                                style="color:#ffc000; font-size: 25px;"></i>&nbsp;&nbsp;
-                Report patient</a></h2>
+               href="/patient_history_print"><img src="{{ asset('img/patient_report.png') }}" class="pull-left" width="40px"/>&nbsp;&nbsp;Report patient</a></h2>
     </div>
 
 </br>

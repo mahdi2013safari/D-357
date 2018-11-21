036230e446ba836acc20c56d8df6535ba0d887ab
Mohammad-jawad <sakhizadah1468@gmail.com>
2018-11-21 09:15:40 +0430

    the two part teeth shade and type added

---
+++ app/Http/Controllers/TeethCoverTypeController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TeethCoverType;
+use Illuminate\Http\Request;
+
+class TeethCoverTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $teethcover=TeethCoverType::orderBy('id','asc')->paginate(10);
+        return view('teethcover',compact('teethcover'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $teethcover=new TeethCoverType();
+        $teethcover->type=$request->cover;
+        $teethcover->price=$request->price;
+        $teethcover->save();
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TeethCoverType  $teethCoverType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TeethCoverType $teethCoverType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TeethCoverType  $teethCoverType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TeethCoverType $teethCoverType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TeethCoverType  $teethCoverType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TeethCoverType $teethCoverType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TeethCoverType  $teethCoverType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $delete=TeethCoverType::find($id);
+        $delete->delete();
+        return redirect()->back();
+    }
+}
